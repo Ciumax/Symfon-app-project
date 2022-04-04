5b71e8dcb22950eea2d72e4a05835e2a8d8e1083
Tomasz <ciumek2991@gmail.com>
2022-04-04 09:45:28 +0200

    End of tutorial

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CommentController extends AbstractController
@@ -11,12 +12,14 @@ class CommentController extends AbstractController
     /**
      * @Route("/comments/{id}/vote/{direction<up|down>}", methods = "POST")
      */
-    public function commentVote($id, $direction)
+    public function commentVote($id, $direction, LoggerInterface $logger)
     {
         if($direction =='up')
         {
             $currentVoteCount = rand(7,100);
+            $logger->info("vote up!");
         } else {
+            $logger->info("Vote down!");
             $currentVoteCount = rand(0, 5);
         }
 
